f37ce54b426c621d5858505bd4491085e349858e
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-09-29 15:43:39 -0700

    Add TodoItem.module.css file

--- react-todo-app/src/components/TodosList.js
+++ react-todo-app/src/components/TodosList.js
@@ -1,16 +1,25 @@
-import React from "react"
-import TodoItem from "./TodoItem";
+import React from 'react';
+import TodoItem from './TodoItem';
+/* eslint-disable react/prop-types */
 
-class TodosList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map(todo => (
-          <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
-        ))}
-      </ul>
-    )
-  }
-}
+const TodosList = (props) => {
+  const { todos } = props;
+  const { setUpdate } = props;
+  const { handleChangeProps } = props;
+  const { deleteTodoProps } = props;
+  return (
+    <ul className="todo-wrapper">
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+          setUpdate={setUpdate}
+        />
+      ))}
+    </ul>
+  );
+};
 
-export default TodosList
+export default TodosList;
